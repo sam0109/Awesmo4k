cc873f689fb8941f5eb0f6bfe93a64469be0eeab
Sam Sobell <sam.sobell@gmail.com>
2013-12-27 19:12:12 -0800

    Bed
    Added a Bed object that changes the animation of the player when he is
    in range and presses e. Placeholder animation and no texture.

--- Assets/Scripts/Manny/MannyStats.cs
+++ Assets/Scripts/Manny/MannyStats.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class MannyStats : MonoBehaviour {
 
 	public float health = 100;
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -21,4 +22,14 @@ public class MannyStats : MonoBehaviour {
 			Destroy(coll.gameObject);
 		}
 	}
+	public void Sleeping(){
+		anim.SetBool ("Sleeping", true);
+		if (Input.GetKeyDown ("space") || Input.GetKeyDown ("a") || Input.GetKeyDown ("d")) {
+			anim.SetBool ("Sleeping", false);
+		}
+	}
+	public float getPositionX(){
+		return transform.position.x;
+	}
+
 }

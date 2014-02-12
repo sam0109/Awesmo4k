a3ea15a7f1ef905baf67d48b4236a970dcf75581
Sam Sobell <sam.sobell@gmail.com>
2014-02-11 19:40:52 -0800

    Added Level 1!

---
+++ Assets/Scripts/Manny/SkyMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyMover : MonoBehaviour {
+	public GameObject Manny;
+	float oldX;
+	float oldY;
+	public float xParallaxFactor = 2;
+	public float yParallaxFactor = 2;
+	// Use this for initialization
+	void Start () {
+		transform.position = Manny.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2((Manny.transform.position.x - oldX) / xParallaxFactor, (Manny.transform.position.y - oldY) / yParallaxFactor);
+		oldX = transform.position.x;
+		oldY = transform.position.y;
+	}
+	
+}
